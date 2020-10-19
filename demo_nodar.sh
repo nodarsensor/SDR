340afad669d4b2f5c9bbe2e9313d3ba5406021db
Piotr Swierczynski <pwswierczynski@gmail.com>
2020-10-19 17:01:22 -0400

    Updated demo script

--- demo_nodar.sh
+++ demo_nodar.sh
@@ -1,8 +1,1 @@
-bin="$(pwd)/build/bin/fdr"
-datasetroot="$(pwd)/data"
-resultsroot="$(pwd)/results/nodar/disp_0"
-
-echo ${bin}
-
-mkdir -p ${resultsroot}
-${bin} -targetDir ${datasetroot}/nodar -outputDir ${resultsroot} -mode NODAR -lambda 0.30 -seg_k 80.0 -inlier_ratio 0.50 -ndisp 256 -use_swap false
+build/bin/fdr -imagePath data/example/left_image.png -disparityPath data/example/disparity.png -outputPath data/example/sdr_result.png -lambda 0.30 -seg_k 80.0 -inlier_ratio 0.50 -ndisp 256 -use_swap false
